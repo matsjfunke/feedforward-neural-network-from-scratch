85e9547643f48a573535431470436f04603654d9
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-06-14 18:15:58 +0200

    input data prep complete

--- mnist.py
+++ mnist.py
@@ -18,23 +18,36 @@ train_labels = np.array(train_data.loc[:, 'label'])
 train_data = np.array(train_data.loc[:, train_data.columns != 'label'])
 
 
-# uncomment to visualize the input data. change  index to view other numbers
+# uncomment to visualize the input number. change  index to view other numbers
 # index = 7
 # plt.title((train_labels[index]))
 # plt.imshow(train_data[index].reshape(28, 28), cmap="binary")
 # plt.show()
 
-# uncomment to view distribution of numbers 0-9 accross training set
-print("train data")
-y_value = np.zeros((1, 10))
-for i in range(10):
-    print("occurance of ", i, "=", np.count_nonzero(train_labels == i))
-    y_value[0, i-1] = np.count_nonzero(train_labels == i)
 
-y_value = y_value.ravel()
-x_value = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-plt.xlabel('number label')
-plt.ylabel('count')
-plt.bar(x_value, y_value, 0.7, color='r')
-plt.xticks(x_value)
-plt.show()
+# uncomment to view distribution of numbers 0-9 accross training set
+# print("train data")
+# y_value = np.zeros((1, 10))
+# for i in range(10):
+#     print("occurance of ", i, "=", np.count_nonzero(train_labels == i))
+#     y_value[0, i-1] = np.count_nonzero(train_labels == i)
+#
+# y_value = y_value.ravel()
+# x_value = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# plt.xlabel('number label')
+# plt.ylabel('count')
+# plt.bar(x_value, y_value, 0.7, color='r')
+# plt.xticks(x_value)
+# plt.show()
+
+
+# ensuring that train_data is a vector, and train_label is a matrix of 0's and 1's
+train_data = np.reshape(train_data, [784, 42000])  # reshapes the training data, each image: originally 28x28 pixels is flattened into vector of 784 elements
+train_label = np.zeros((10, 42000))  # one-hot encoding transforms categorical labels into  binary format
+for col in range(42000):
+    val = train_labels[col]
+    for row in range(10):
+        if (val == row):
+            train_label[val, col] = 1
+print("train_data shape=" + str(np.shape(train_data)))
+print("train_label shape=" + str(np.shape(train_label)))
