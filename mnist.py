97386d4f8d10a7250abac7072bad901183898ca5
Mats Julius Funke <125814808+matsjfunke@users.noreply.github.com>
2024-06-14 19:32:42 +0200

    activation function + dervation of them

--- mnist.py
+++ mnist.py
@@ -18,27 +18,27 @@ train_labels = np.array(train_data.loc[:, 'label'])
 train_data = np.array(train_data.loc[:, train_data.columns != 'label'])
 
 
-# uncomment to visualize the input number. change  index to view other numbers
-# index = 7
-# plt.title((train_labels[index]))
-# plt.imshow(train_data[index].reshape(28, 28), cmap="binary")
-# plt.show()
+# input number visualization.
+index = 7  # change  index to view other numbers
+plt.title((train_labels[index]))
+plt.imshow(train_data[index].reshape(28, 28), cmap="binary")
+plt.show()
 
 
-# uncomment to view distribution of numbers 0-9 accross training set
-# print("train data")
-# y_value = np.zeros((1, 10))
-# for i in range(10):
-#     print("occurance of ", i, "=", np.count_nonzero(train_labels == i))
-#     y_value[0, i-1] = np.count_nonzero(train_labels == i)
-#
-# y_value = y_value.ravel()
-# x_value = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-# plt.xlabel('number label')
-# plt.ylabel('count')
-# plt.bar(x_value, y_value, 0.7, color='r')
-# plt.xticks(x_value)
-# plt.show()
+# distribution of numbers 0-9 accross training set
+print("train data")
+y_value = np.zeros((1, 10))
+for i in range(10):
+    print("occurance of ", i, "=", np.count_nonzero(train_labels == i))
+    y_value[0, i-1] = np.count_nonzero(train_labels == i)
+
+y_value = y_value.ravel()
+x_value = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+plt.xlabel('number label')
+plt.ylabel('count')
+plt.bar(x_value, y_value, 0.7, color='r')
+plt.xticks(x_value)
+plt.show()
 
 
 # ensuring that train_data is a vector, and train_label is a matrix of 0's and 1's
@@ -51,3 +51,37 @@ for col in range(42000):
             train_label[val, col] = 1
 print("train_data shape=" + str(np.shape(train_data)))
 print("train_label shape=" + str(np.shape(train_label)))
+
+
+# activation functions (forward propagation)
+# Z: linear transformation (pre-activation value)
+def sigmoid(Z):
+    activation_function = 1 / (1 + np.exp(-Z))
+    cache = Z
+    return activation_function, cache
+
+
+def relu(Z):
+    activation_function = np.maximum(0, Z)
+    cache = Z
+    return activation_function, cache
+
+
+# derivative of activation function (backward propagation)
+# remember gradients are used to find the minimum
+def sigmoid_gradient(activation_fn_gradient, cache):
+    Z = cache
+    sigmoid_Z = 1 / (1 + np.exp(-Z))
+    linear_transformation_gradient = activation_fn_gradient * sigmoid_Z * (1 - sigmoid_Z)
+
+    assert (linear_transformation_gradient.shape == Z.shape)
+    return linear_transformation_gradient
+
+
+def relu_gradient(activation_fn_gradient, cache):
+    Z = cache
+    linear_transformation_gradient = np.array(activation_fn_gradient, copy=True)
+    linear_transformation_gradient[Z <= 0] = 0
+
+    assert (linear_transformation_gradient.shape == Z.shape)
+    return linear_transformation_gradient
